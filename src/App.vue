8589796c17f11eee46e9bc3d8a6e4d9e91d81464
Pawan918 <pawanshah84489@gmail.com>
2023-09-29 17:41:57 +0530

    name displayed on input

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Test msg="wait"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+// import { ref } from 'vue';
+import Test from './components/Test.vue';
+// const message = ref('');
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    Test,
+}
 }
 </script>
 
---
+++ src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="test">
+        {{ msg }}
+        <br/>
+        <br/>
+        <input type="text" v-model="name"/>
+    </div>
+</template>
+
+<script>
+export default{
+    name : 'TestComp',
+    data : ()=>{
+        return {
+            name : "manish1"
+        }
+    }
+}
+</script>
